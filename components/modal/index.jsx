c0940139d6085c67f15b4b5f5d19c4e7d1cdaab0
Asadbek <asadbeknan5566@gmail.com>
2022-12-27 20:37:04 +0500

    ref: modal

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -1,27 +1,21 @@
 import React, { useRef, useEffect } from "react";
 import Input from "../Input";
 import Menu from "../eachMenu";
-import blog from "../../public/icons/blog.svg";
-import search from "../../public/icons/search.svg";
+import { menu } from "../../data/menu";
 
 const Modal = ({ modalStatus }) => {
   const inputRef = useRef(null);
+
   useEffect(() => {
     if (modalStatus) inputRef.current.focus();
   }, [modalStatus]);
+
   return (
     <div className="modal">
       <Input placeholder="Type a command or search..." ref={inputRef} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Search Blog..." icon={search} shortcut />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
-      <Menu title="Blog" icon={blog} />
+      {menu.map(({ id, title, icon, shortcut }) => (
+        <Menu key={id} title={title} icon={icon} shortcut={shortcut} />
+      ))}
     </div>
   );
 };
---
+++ data/menu.js
@@ -0,0 +1,46 @@
+import blog from "../public/icons/blog.svg";
+import search from "../public/icons/search.svg";
+export const menu = [
+  {
+    id: 1,
+    title: "Blog",
+    icon: blog,
+    shortcut: false,
+  },
+  {
+    id: 2,
+    title: "Search Blog...",
+    icon: search,
+    shortcut: true,
+  },
+  {
+    id: 3,
+    title: "Blog",
+    icon: blog,
+    shortcut: false,
+  },
+  {
+    id: 4,
+    title: "Blog",
+    icon: blog,
+    shortcut: false,
+  },
+  {
+    id: 5,
+    title: "Blog",
+    icon: blog,
+    shortcut: false,
+  },
+  {
+    id: 6,
+    title: "Blog",
+    icon: blog,
+    shortcut: false,
+  },
+  {
+    id: 7,
+    title: "Blog",
+    icon: blog,
+    shortcut: false,
+  },
+];
