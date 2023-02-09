79465cd7703d36fd79c6695d244a361d7c11963e
Asadbek <asadbeknan5566@gmail.com>
2023-02-09 11:59:15 +0500

    fixed

--- data/projects.js
+++ data/projects.js
@@ -8,34 +8,34 @@ import studiio from "../public/photos/studiio.png";
 import cyber from "../public/photos/main-page.5a6d477d.png";
 
 export const projects = [
-  {
-    id: 1,
-    title: "Lenta.uz",
-    desc: "Hammasi emas eng keraklisi",
-    img: lentaDark,
-    slug: "https://lenta.uz/",
-  },
-  {
-    id: 2,
-    title: "Jobhunt.uz",
-    desc: "Найти работу в Ташкенте и в Узбекистане",
-    img: job,
-    slug: "https://jobhunt.uz/",
-  },
-  {
-    id: 3,
-    title: "STesting.uz",
-    desc: "STesting is a testing platform for students to prepare for International Studies of PISA, TIMSS, PIRLS.",
-    img: stesting,
-    slug: "https://stesting.uz/en",
-  },
-  {
-    id: 4,
-    title: "Ricomel.com",
-    desc: "Ricomel beverages (Dinay)",
-    img: ricomel,
-    slug: "https://ricomel.com/uz",
-  },
+  // {
+  //   id: 1,
+  //   title: "Lenta.uz",
+  //   desc: "Hammasi emas eng keraklisi",
+  //   img: lentaDark,
+  //   slug: "https://lenta.uz/",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Jobhunt.uz",
+  //   desc: "Найти работу в Ташкенте и в Узбекистане",
+  //   img: job,
+  //   slug: "https://jobhunt.uz/",
+  // },
+  // {
+  //   id: 3,
+  //   title: "STesting.uz",
+  //   desc: "STesting is a testing platform for students to prepare for International Studies of PISA, TIMSS, PIRLS.",
+  //   img: stesting,
+  //   slug: "https://stesting.uz/en",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Ricomel.com",
+  //   desc: "Ricomel beverages (Dinay)",
+  //   img: ricomel,
+  //   slug: "https://ricomel.com/uz",
+  // },
   {
     id: 5,
     title: "Haval.uz",
@@ -57,11 +57,11 @@ export const projects = [
     img: studiio,
     slug: "https://studiio.vercel.app/",
   },
-  {
-    id: 10,
-    title: "Cyberpark",
-    desc: "ИННОВАЦИОННЫЙ IT-КЛАСТЕР В УЗБЕКИСТАНЕ",
-    img: cyber,
-    slug: "https://cyberpark.uz/",
-  },
+  // {
+  //   id: 10,
+  //   title: "Cyberpark",
+  //   desc: "ИННОВАЦИОННЫЙ IT-КЛАСТЕР В УЗБЕКИСТАНЕ",
+  //   img: cyber,
+  //   slug: "https://cyberpark.uz/",
+  // },
 ];
