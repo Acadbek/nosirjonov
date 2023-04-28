70a86c369191f8c3cc01f09b14e400348ede12d0
asadbek <ibnnumon@gmail.com>
2023-04-28 16:25:58 +0500

    ...

--- data/projects.js
+++ data/projects.js
@@ -9,20 +9,20 @@ import cyber from '../public/photos/main-page.5a6d477d.png';
 import crm from '../public/photos/snowCrm.png';
 
 export const projects = [
-  {
-    id: 5,
-    title: 'Haval.uz',
-    desc: "GWM endi O'zbekistonda!",
-    img: haval,
-    slug: 'https://haval.uz/uz',
-  },
-  {
-    id: 1,
-    title: 'Lenta.uz',
-    desc: 'Hammasi emas eng keraklisi',
-    img: lentaDark,
-    slug: 'https://lenta.uz/',
-  },
+  // {
+  //   id: 5,
+  //   title: 'Haval.uz',
+  //   desc: "GWM endi O'zbekistonda!",
+  //   img: haval,
+  //   slug: 'https://haval.uz/uz',
+  // },
+  // {
+  //   id: 1,
+  //   title: 'Lenta.uz',
+  //   desc: 'Hammasi emas eng keraklisi',
+  //   img: lentaDark,
+  //   slug: 'https://lenta.uz/',
+  // },
   // {
   //   id: 2,
   //   title: "Jobhunt.uz",
@@ -30,34 +30,34 @@ export const projects = [
   //   img: job,
   //   slug: "https://jobhunt.uz/",
   // },
-  {
-    id: 3,
-    title: 'STesting.uz',
-    desc: 'STesting is a testing platform for students to prepare for International Studies of PISA, TIMSS, PIRLS.',
-    img: stesting,
-    slug: 'https://stesting.uz/en',
-  },
-  {
-    id: 4,
-    title: 'Ricomel.com',
-    desc: 'Ricomel beverages (Dinay)',
-    img: ricomel,
-    slug: 'https://ricomel.com/uz',
-  },
-  {
-    id: 77,
-    title: 'Houzing',
-    desc: 'Skyper Pool Partment',
-    img: houzing,
-    slug: 'https://rent-houze.netlify.app/',
-  },
-  {
-    id: 9,
-    title: 'Snow CRM SYSTEM',
-    desc: 'Customer Relationship Management.',
-    img: crm,
-    slug: 'https://test-nosirjonov.vercel.app/home',
-  },
+  // {
+  //   id: 3,
+  //   title: 'STesting.uz',
+  //   desc: 'STesting is a testing platform for students to prepare for International Studies of PISA, TIMSS, PIRLS.',
+  //   img: stesting,
+  //   slug: 'https://stesting.uz/en',
+  // },
+  // {
+  //   id: 4,
+  //   title: 'Ricomel.com',
+  //   desc: 'Ricomel beverages (Dinay)',
+  //   img: ricomel,
+  //   slug: 'https://ricomel.com/uz',
+  // },
+  // {
+  //   id: 77,
+  //   title: 'Houzing',
+  //   desc: 'Skyper Pool Partment',
+  //   img: houzing,
+  //   slug: 'https://rent-houze.netlify.app/',
+  // },
+  // {
+  //   id: 9,
+  //   title: 'Snow CRM SYSTEM',
+  //   desc: 'Customer Relationship Management.',
+  //   img: crm,
+  //   slug: 'https://test-nosirjonov.vercel.app/home',
+  // },
   // {
   //   id: 10,
   //   title: "Cyberpark",
