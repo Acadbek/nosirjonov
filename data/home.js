41303a7550f74c24ae35117f51ea810580062c6b
asad <87940040+Acadbek@users.noreply.github.com>
2024-07-05 18:05:01 +0500

    Update home.js

--- data/home.js
+++ data/home.js
@@ -4,7 +4,7 @@ export const data = [
     title: "Asadbek Nosirjonov",
     desc1:
       "Junior Developer, JS enthusiast, practicing minimalist, and electronic lover in search of good shortcut. Writing about my coding style and some extra shit.",
-    desc2: "Working at Najot ta’lim to make Uzbekistan more survivable.",
+    desc2: "Working at Blaze.ERP.",
     desc3:
       "⌘K or Alt+K to let the dark magic happen or press the ⌘ button on navigation",
   },
