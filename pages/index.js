f1f027f8a0fc0e760770a430b6bf8d3cb6dada27
asad <87940040+Acadbek@users.noreply.github.com>
2024-07-05 18:05:40 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         good shortcut. Writing about my coding style and some extra shit interested in low level languages
       </p>
       <p className="text-[#fff] leading-[35px] text-[20px] tracking-wide font-[500] mt-[20px]">
-        Working at Najot ta&rsquo;lim to make Uzbekistan more survivable.
+        Working at Blaze.ERP
       </p>
       <p className="text-[#fff] leading-[35px] text-[20px] tracking-wide font-[400] mt-[50px]">
         ⌘K or Alt+K to let the dark magic happen or press the ⌘ button on
