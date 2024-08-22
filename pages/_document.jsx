db472a8bc4b036006011d3308868277c311643b8
Asadbek <asadbeknan5566@gmail.com>
2024-08-22 10:06:06 +0500

    feat: icones

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    (function() {
+                      function updateFavicon() {
+                        const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                        const faviconLink = document.querySelector('link[rel="icon"]');
+                        faviconLink.href = isDarkMode ? '/favicon-dark.png' : '/favicon-light.png';
+                      }
+                      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateFavicon);
+                      updateFavicon();
+                    })();
+                  `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
