63392cd4e891b6941e8e5b11d9757bb0a8659c70
Asad <ibnnumon@gmail.com>
2025-01-05 17:35:05 +0500

    ref: 404

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
         enthusiast, practicing minimalist, and electronic lover in search of
         good shortcut. Writing about my coding style and some extra shit interested in low level languages
       </p>
-      <p className="text-[#fff] leading-[35px] text-[20px] tracking-wide font-[500] mt-[20px]">
+      {/* <p className="text-[#fff] leading-[35px] text-[20px] tracking-wide font-[500] mt-[20px]">
         Working at Blaze.ERP
-      </p>
+      </p> */}
       <p className="text-[#fff] leading-[35px] text-[20px] tracking-wide font-[400] mt-[50px]">
         ⌘K or Alt+K to let the dark magic happen or press the ⌘ button on
         navigation
