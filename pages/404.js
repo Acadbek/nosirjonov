29e7788b93ef205c468234e502daa2928be7b47e
Asad <ibnnumon@gmail.com>
2025-01-05 17:28:13 +0500

    ref: 404

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,15 @@
 import React from "react";
 import img from "../public/photos/comingSoon.jpg";
 import Image from "next/image";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="container">
-      <Image src={img} width={750} alt="Coming Soon" />
+    <div className="container h-[80vh] flex items-center justify-center">
+      <div className="flex flex-col gap-3">
+        <p className="text-3xl bg-white text-black px-2">404</p>
+        <Link className="text-2xl flex items-center justify-center p-1 border-[0.5px] roun border-transparent hover:border-cyan-400" href={'/'}>./</Link>
+      </div>
     </div>
   );
 };
